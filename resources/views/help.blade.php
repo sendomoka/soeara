ac5ede939f85a838e8d79ff2a15be093397c859c
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-01 17:23:56 +0700

    docs: update README.md

---
+++ resources/views/help.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Help</title>
+</head>
+<body>
+    <h1>Reference</h1>
+    <ol>
+        <li>Filament - Admin Builder <a href="https://filamentphp.com/docs/3.x/panels/installation">https://filamentphp.com/docs/3.x/panels/installation</a></li>
+        <li>Craftable - CRUD Instant Builder <a href="https://github.com/BRACKETS-by-TRIAD/craftable">https://github.com/BRACKETS-by-TRIAD/craftable</a></li>
+        <li>Sanctum - Auth Management <a href="https://laravel.com/docs/10.x/sanctum">https://laravel.com/docs/10.x/sanctum</a></li>
+        <li>Debugbar - Alur Queries <a href="https://github.com/barryvdh/laravel-debugbar">https://github.com/barryvdh/laravel-debugbar</a></li>
+    </ol>
+</body>
+</html>
