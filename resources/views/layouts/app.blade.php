3440c4105d19a4bb8e5ede66c0d5cfd0e2d0d426
Jehian Athaya <55863992+sendomoka@users.noreply.github.com>
2024-03-20 10:41:17 +0700

    feat: add tailwind css and setup background color

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        @vite('resources/css/app.css')
+    </head>
+<body class="w-full min-h-dvh bg-gradient-to-b from-[#960707] to-[#07041F] text-white p-4">
+    @yield('content')
+</body>
+</html>
